5505d7c315226f285419720801621630c0573696
capitalDev22 <capital.aluglass@gmail.com>
2025-09-10 18:24:16 +0300

    Delete notes.js

--- notes.js
+++
@@ -1,4 +0,0 @@
-// Only include stack in development for security //calculationController.js
-// Calculate button must always be hit before add to cart
-//checks if user already has a cart in the db
-// Anoying Item added to cart! prompt
